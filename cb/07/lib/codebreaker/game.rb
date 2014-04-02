7b13c806fc8694690eeed772cc7d978e6433fdbf
navpreetsingh <navpreet.singh.k@gmail.com>
2014-04-02 19:37:59 +0530

    chapter-9

--- cb/07/lib/codebreaker/game.rb
+++ cb/07/lib/codebreaker/game.rb
@@ -14,13 +14,31 @@ module Codebreaker
     end
     
     def start (secret=nil)
+    	@secret = secret
     	@output.puts "Welcome to Codebreaker!"
     	@output.puts "Enter guess:"
     end
     
     def guess(guess)
-    
+    	mark = ""
+        (0..3).each do |index|
+            if exact_match?(guess, index)
+    		  mark << "+"
+            elsif number_match?(guess, index) 
+                mark << "-"
+            end
+
+    	end    	
+    	@output.puts mark.split("").sort.join
+    end    
+    def exact_match?(guess, index)
+        guess[index] == @secret[index]
     end
-    
+    def number_match?(guess, index)
+        value = guess[index]        
+        @secret.include?(guess[index]) && !exact_match?(guess, index) && @secret.count(value) >= guess.count(value)
+
+    end
   end
 end
+
